e529f66296639b3f8a6616e93062b9b54f977eff
jecAmorim <jeciane_amorim@hotmail.com>
2020-05-05 19:25:14 -0300

    menu ajustado

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import SideNav, {Header, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import {FiUser,FiHome,FiBook,FiImage,FiFileText,FiHelpCircle,FiLogOut,FiSettings} from "react-icons/fi/";
@@ -13,101 +13,97 @@ export default function Footer({title,children}) {
     const history=useHistory();
 
     return (
-        <SideNav
-        onSelect={(selected) => {
-            // Add your code here
-        }}
-        style={{background: '#12524B'}}
+    <SideNav
+        onSelect={(selected) => { }}
+        style={{background: '#12524B', padding:'5Px'}}
     >
-        <SideNav.Toggle />
-        <SideNav.Nav defaultSelected="home">
-            <NavItem eventKey="icon">
-                <img src={logoImg} alt="Logo Menu"/>
-            </NavItem>
-            <NavItem eventKey="user">
-                <FaUserCircle size={48} color='#fff'/>
-            </NavItem>
-            <NavItem eventKey="home">
-                <NavIcon>
-                </NavIcon>
-                <NavText>
-                    Home
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="inicio">
-                <NavIcon>
-                    <FiHome size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Inicio
-                </NavText>
-                <NavItem eventKey="charts/linechart">
-                    <NavText>
-                        Line Chart
-                    </NavText>
-                </NavItem>
-            </NavItem>
-            <NavItem eventKey="album-fotografias">
-                <NavIcon>
-                    <FiBook size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Albúm fotografias
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="fotografias">
-                <NavIcon>
-                    <FiImage size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Fotografias
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="usuario"
-                onClick={() => {
-                    history.push('/users');
-                 }}
-            >
-                <NavIcon>
-                    <FiUser size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                Usuarios
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="Relatorios">
-                <NavIcon>
-                    <FiFileText size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                   Relatorios
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="configuracoes">
-                <NavIcon>
-                    <FiSettings size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Configurações
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="sair">
-                <NavIcon>
-                    <FiLogOut size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Sair
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="help">
-                <NavIcon>
-                    <FiHelpCircle size={48} color='#fff'/>
-                </NavIcon>
-                <NavText>
-                    Ajuda
-                </NavText>
-            </NavItem>
-        </SideNav.Nav>
+        <div className="side-nav">
+            <div eventKey="icon">
+                <img className="navItem-logo" src={logoImg} alt="Logo Menu"/>
+            </div>
+            <div eventKey="user" className="navItem-perfile">
+                <FaUserCircle className="navItem-perfile icon" size={38} color='#fff'/>
+                <h2>{"Administrador"}</h2>
+                <h3>{"admin@siagaf.com"}</h3>
+                <hr size="1" width="95%"></hr>                
+            </div>        
+            <div className="nav-body">
+                <SideNav.Nav>
+                    <NavItem eventKey="inicio">
+                        <NavIcon className="icon">
+                            <FiHome size={40} color='#fff' margin="5px"/>
+                            <h2>{"Inicio"}</h2>
+                        </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="album-fotografias">
+                        <NavIcon className="icon">
+                            <FiBook  size={40} color='#fff' margin="5px"/>
+                            <h2>{"Albúm"}<br></br>{"fotografias"}</h2>
+                        </NavIcon>
+                        <NavItem eventKey="charts/linechart">
+                            <NavText>
+                                Line Chart
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="charts/linechart">
+                            <NavText>
+                                Line Chart
+                            </NavText>
+                        </NavItem>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="fotografias">
+                        <NavIcon className="icon">
+                            <FiImage  size={40} color='#fff' margin="5px"/>
+                            <h2>{"Fotografias"}</h2>
+                        </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="usuario"
+                        onClick={() => {
+                            history.push('/users');
+                        }}
+                    >
+                        <NavIcon className="icon">
+                            <FiUser size={40} color='#fff' margin="5px"/>
+                            <h2>{"Usuarios"}</h2>
+                        </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="Relatorios">
+                    <NavIcon className="icon">
+                            <FiFileText size={40} color='#fff' margin="5px"/>
+                            <h2>{"Relatorios"}</h2>
+                        </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="configuracoes">
+                        <NavIcon className="icon">
+                            <FiSettings size={40} color='#fff'/>
+                            <h2>{"Configurações"}</h2>
+                        </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem eventKey="sair">
+                        <NavIcon  className="icon">
+                            <FiLogOut  size={40} color='#fff'/>
+                            <h2>{"Sair"}</h2>
+                    </NavIcon>
+                    </NavItem>
+                    <br></br>
+                    <NavItem  eventKey="help">
+                        <NavIcon> 
+                            <div className="icon-footer">
+                                <FiHelpCircle size={40} color='#fff'/>
+                                <h2>{"Ajuda"}</h2>
+                            </div>
+                        </NavIcon>
+                    </NavItem>
+                </SideNav.Nav>
+            </div>
+        </div>
     </SideNav>
+
     );
   }
