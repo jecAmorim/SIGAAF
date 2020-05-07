ff1402ee2d09715987ab97e7e33c47e0b03a6956
DanielCabral <Daniel-cabral14@hotmail.com>
2020-05-06 22:42:21 -0300

    registrar album front

--- back-end/app/models/albums.js
+++ back-end/app/models/albums.js
@@ -1,11 +1,16 @@
 
 const models = require('../models');
-
+const moment = require('moment');
 module.exports = (sequelize, DataTypes) => {
     const Album = sequelize.define('Album', {
       album_titulo: DataTypes.STRING,
       album_descricao: DataTypes.STRING,
-      album_data_aquisicao: DataTypes.DATE,
+      album_data_aquisicao: {
+        type: DataTypes.DATE, 
+        get() {
+        return moment(this.getDataValue('album_data_aquisicao')).format('YYYY-MM-DD');
+        }
+      },
       album_estado_conservacao: DataTypes.STRING,
       LibraryId: {
         type: DataTypes.INTEGER,
